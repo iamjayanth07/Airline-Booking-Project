4e24ff249c64b68b0c149d9ea13dbd453222ac45
iamjayanth07 <jayanth2760naik@gmail.com>
2025-11-03 09:44:12 +0530

    Added basic sequelize and cli related files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express= require('express');
 
 const v1routes=require('./v1');
+// const v2routes=require('./v2');
 
 const routes=express.Router();
 
 routes.use('/v1',v1routes);
+// routes.use('/v2',v2routes);
 
 module.exports=routes;
